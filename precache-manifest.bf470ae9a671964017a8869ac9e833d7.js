5f81fa6712e34c01dfe0c0c67776a0d18824dae1
ramHYHosakote <reach.rambymail@gmail.com>
2022-11-19 01:44:46 +0530

    Updates

---
+++ precache-manifest.bf470ae9a671964017a8869ac9e833d7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3f6608cc49ebd9fa8d5",
+    "url": "/resume/static/css/main.222ecf2c.chunk.css"
+  },
+  {
+    "revision": "c3f6608cc49ebd9fa8d5",
+    "url": "/resume/static/js/main.0cda605f.chunk.js"
+  },
+  {
+    "revision": "131b33b8ae6d8968cdb1",
+    "url": "/resume/static/js/runtime~main.b75e7171.js"
+  },
+  {
+    "revision": "bf1f521398cc9fed98ad",
+    "url": "/resume/static/js/2.52f8a0f4.chunk.js"
+  },
+  {
+    "revision": "39196528abd05fc44569",
+    "url": "/resume/static/js/3.272b0cae.chunk.js"
+  },
+  {
+    "revision": "29df7aca69a0ce1f086c5671e4b44980",
+    "url": "/resume/static/media/profile-photo.29df7aca.jpg"
+  },
+  {
+    "revision": "f482d38d962b4d95853bef956ff6dd83",
+    "url": "/resume/static/media/Inter-Thin.f482d38d.ttf"
+  },
+  {
+    "revision": "3e87064b7567bef4ecd2ba977ce028bc",
+    "url": "/resume/static/media/Inter-SemiBold.3e87064b.ttf"
+  },
+  {
+    "revision": "eba360005eef21ac6807e45dc8422042",
+    "url": "/resume/static/media/Inter-Regular.eba36000.ttf"
+  },
+  {
+    "revision": "1aa99aa25c72307cb7f16fae35e8c9d1",
+    "url": "/resume/static/media/Inter-Medium.1aa99aa2.ttf"
+  },
+  {
+    "revision": "d4be01c95657978131342b1dcf829a45",
+    "url": "/resume/static/media/Inter-Light.d4be01c9.ttf"
+  },
+  {
+    "revision": "819a76705047d6474cb529a319e74bbc",
+    "url": "/resume/static/media/Inter-ExtraLight.819a7670.ttf"
+  },
+  {
+    "revision": "c6dacb6bcfcd747bba440bf2fbd2c85a",
+    "url": "/resume/static/media/Inter-Black.c6dacb6b.ttf"
+  },
+  {
+    "revision": "82c8c1cf127220ccd9914e5dc739de28",
+    "url": "/resume/static/media/Inter-ExtraBold.82c8c1cf.ttf"
+  },
+  {
+    "revision": "cef517a165e8157d9f14a0911190948d",
+    "url": "/resume/static/media/Inter-Bold.cef517a1.ttf"
+  },
+  {
+    "revision": "488d21e7316c31fab59d4bb4714c3af3",
+    "url": "/resume/index.html"
+  }
+];
